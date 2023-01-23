54539995498c7c43161d5dc4789bd99a16c6acd4
imMatheus <matheusmendesbarata03@gmail.com>
2023-01-23 11:17:37 +0100

    added sidebar

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -5,6 +5,13 @@ export default function SideNav() {
     return (
         <div className='sidenav'>
             <p>ALL BOARDS (2)</p>
+
+            <ul>
+                <li className=''>Kanban board</li>
+                <li className=''>Kanban board</li>
+                <li className='active'>Kanban board</li>
+                <li className=''>Kanban board</li>
+            </ul>
         </div>
     )
 }
