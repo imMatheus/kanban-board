ffa1effc31fd6d0ddf7d9376feffca61e2da9cb0
imMatheus <matheusmendesbarata03@gmail.com>
2023-01-23 10:41:18 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SideNav from './components/SideNav/SideNav'
+
+function App() {
+    return (
+        <div className='App'>
+            <SideNav />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/SideNav/SideNav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SideNav.scss'
+
+export default function SideNav() {
+    return (
+        <div className='sidenav'>
+            <p>ALL BOARDS (2)</p>
+        </div>
+    )
+}
