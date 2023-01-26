9bcaa9ba627e6eeea2a75207da2ddfe1fdf0d01e
imMatheus <matheusmendesbarata03@gmail.com>
2023-01-26 16:38:00 +0100

    added dnd functionality

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -2,27 +2,84 @@ import React from 'react'
 import './Board.scss'
 import Ticket from '../Ticket/Ticket'
 import { useProject } from '../../context/ProjectContext'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 
 export default function Board() {
     const { currentProject } = useProject()
 
+    function onDragEnd(result) {
+        console.log(result)
+
+        if (!result.destination) return
+
+        const { source, destination } = result
+
+        const board = [...currentProject.board]
+        const sourceIndex = board.findIndex(
+            (column) => column.name === source.droppableId
+        )
+        const destinationIndex = board.findIndex(
+            (column) => column.name === destination.droppableId
+        )
+        const [removedTicket] = board[sourceIndex].tickets.splice(
+            source.index,
+            1
+        )
+
+        board[destinationIndex].tickets.splice(
+            destination.index,
+            0,
+            removedTicket
+        )
+    }
+
     return (
         <div className='board'>
-            {currentProject.board.map((column) => {
-                return (
-                    <div key={column.name} className='board-column'>
-                        <div className='board-title'>
-                            <div
-                                className={'board-title-icon ' + column.name}
-                            ></div>
-                            <h4>{column.name}</h4>
+            <DragDropContext onDragEnd={onDragEnd}>
+                {currentProject.board.map((column, index) => {
+                    return (
+                        <div key={column.name} id={index}>
+                            <Droppable droppableId={column.name}>
+                                {(provided, snapshot) => (
+                                    <div
+                                        {...provided.droppableProps}
+                                        ref={provided.innerRef}
+                                        active={snapshot.isDraggingOver.toString()}
+                                        className='board-column'
+                                    >
+                                        <div className='board-title'>
+                                            <div
+                                                className={
+                                                    'board-title-icon ' +
+                                                    column.name
+                                                }
+                                            ></div>
+                                            <h4>{column.name}</h4>
+                                        </div>
+                                        {column.tickets.map((ticket, index) => {
+                                            return (
+                                                <Draggable
+                                                    key={ticket.id}
+                                                    draggableId={ticket.id}
+                                                    index={index}
+                                                >
+                                                    {(provided, snapshot) => (
+                                                        <Ticket
+                                                            provided={provided}
+                                                            snapshot={snapshot}
+                                                            item={ticket}
+                                                        />
+                                                    )}
+                                                </Draggable>
+                                            )
+                                        })}
+                                    </div>
+                                )}
+                            </Droppable>
                         </div>
-                        {column.tickets.map((ticket) => {
-                            return <Ticket key={ticket.id} item={ticket} />
-                        })}
-                    </div>
-                )
-            })}
+                    )
+                })}
+            </DragDropContext>
         </div>
     )
 }
